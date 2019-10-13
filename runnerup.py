6aa3b3e2a0f251a490cc9ec341413d58e86d6b81
Aayush Singh <aayush14140151@gmail.com>
2019-10-13 13:16:51 +0530

    Initial

---
+++ runnerup.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    n=int(input("Enter the no of record"))
+for i in range(n):
+    name = input()
+    score = float(input())
+
+result=[]
+for i in range(n):
+    result.append(input(),float(input()))
+print(result)
